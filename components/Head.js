fa9e2b5cb0e69f37718540b9d20cb2f9a73659b7
Goran Jakovljevic <goranefbl@gmail.com>
2018-11-12 21:04:26 +0100

    test3

--- components/Head.js
+++ components/Head.js
@@ -1,8 +1,8 @@
 import Head from 'next/head';
-import '../static/css/style.css';
 
 const HTMLHead = () => (
     <Head>
+        <link href="static/css/style.css" rel="stylesheet" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
         <title>WP + React = Fast :)</title>
